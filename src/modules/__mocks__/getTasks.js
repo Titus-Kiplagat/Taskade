2e911d49837d16ad32d4f92b866c18894d4eaaa1
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-08-24 17:07:15 +0300

    Modify the  mock file by adding objects inside the tasks sample array

--- src/modules/__mocks__/getTasks.js
+++ src/modules/__mocks__/getTasks.js
@@ -1,3 +1,9 @@
-const getTasksMock = [];
+const getTasksMock = [
+  { index: 0, description: 'Task 1', complete: true },
+  { index: 1, description: 'Go for spree shopping on a Friday', complete: false },
+  { index: 2, description: 'Task 3', complete: true },
+  { index: 3, description: 'Buy groceries in the evening', complete: false },
+  { index: 5, description: 'Task 5', complete: true },
+];
 
 export default getTasksMock;
